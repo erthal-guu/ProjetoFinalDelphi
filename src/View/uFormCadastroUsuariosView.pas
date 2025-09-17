550a158f352fd92ee021a4f8437910878a4c4311
Erthal-guu <vplgugs@gmail.com>
2025-09-17 17:26:43 -0300

    Inicio das configurações graficas da tela de crud de usuarios

--- src/View/uFormCadastroUsuariosView.pas
+++ src/View/uFormCadastroUsuariosView.pas
@@ -5,32 +5,34 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Data.DB, Vcl.Grids,
-  Vcl.DBGrids, Vcl.ComCtrls, Vcl.Imaging.pngimage;
+  Vcl.DBGrids, Vcl.ComCtrls, Vcl.Imaging.pngimage, Vcl.StdCtrls, Vcl.Buttons,
+  Vcl.WinXCtrls;
 
 type
   TFormCadastroUsuarios = class(TForm)
     PageControl1: TPageControl;
     Usuarios: TTabSheet;
-    Grupos: TTabSheet;
-    Panel2: TPanel;
     PnlMain: TPanel;
-    PnlButton: TPanel;
-    PnlExcluir: TPanel;
-    PnlEditar: TPanel;
-    PnlEdt: TPanel;
-    PnlAdicionar: TPanel;
     Panel1: TPanel;
-    Panel3: TPanel;
-    PnlSair: TPanel;
-    Panel5: TPanel;
-    Panel6: TPanel;
-    Panel7: TPanel;
-    DBGrid1: TDBGrid;
+    PnlMainButton: TPanel;
+    PnlButton: TPanel;
+    SpeedButton1: TSpeedButton;
+    SpeedButton2: TSpeedButton;
+    SpeedButton3: TSpeedButton;
+    SpeedButton4: TSpeedButton;
+    SpeedButton5: TSpeedButton;
+    SpeedButton6: TSpeedButton;
+    SpeedButton7: TSpeedButton;
+    PnlMainEdit: TPanel;
+    PnlCadastro: TPanel;
+    PnlEdit: TPanel;
+    Edit2: TEdit;
+    Edit1: TEdit;
+    Edit3: TEdit;
     PnlGrid: TPanel;
-    Grid: TDBGrid;
-    PnlRestaurar: TPanel;
-    PnlPesquisar: TPanel;
-    Panel10: TPanel;
+    DBGrid1: TDBGrid;
+    PnlHeader: TPanel;
+    SearchBox1: TSearchBox;
   private
     { Private declarations }
   public
