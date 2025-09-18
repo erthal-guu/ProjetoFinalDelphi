2eda079bf0be13c4622e94aba8982cd6a4cc66f1
erthal-guu <vplgugs@gmail.com>
2025-09-18 17:55:50 -0300

    Commit da configuração grafica da tela de crud

--- src/View/uFormCadastroUsuariosView.pas
+++ src/View/uFormCadastroUsuariosView.pas
@@ -15,6 +15,14 @@ type
     PnlMain: TPanel;
     Panel1: TPanel;
     PnlMainButton: TPanel;
+    PnlMainEdit: TPanel;
+    PnlGrid: TPanel;
+    DBGrid1: TDBGrid;
+    PnlHeader: TPanel;
+    Panel4: TPanel;
+    SearchBox1: TSearchBox;
+    Panel2: TPanel;
+    Panel3: TPanel;
     PnlButton: TPanel;
     SpeedButton1: TSpeedButton;
     SpeedButton2: TSpeedButton;
@@ -23,16 +31,6 @@ type
     SpeedButton5: TSpeedButton;
     SpeedButton6: TSpeedButton;
     SpeedButton7: TSpeedButton;
-    PnlMainEdit: TPanel;
-    PnlCadastro: TPanel;
-    PnlEdit: TPanel;
-    Edit2: TEdit;
-    Edit1: TEdit;
-    Edit3: TEdit;
-    PnlGrid: TPanel;
-    DBGrid1: TDBGrid;
-    PnlHeader: TPanel;
-    SearchBox1: TSearchBox;
   private
     { Private declarations }
   public
